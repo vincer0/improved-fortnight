cd36c882cf9b5cb246c521a60a7d4b45fb0a133a
Vincer0 <pasierb.adam@gmail.com>
2022-04-10 13:03:56 +0200

    chore: small refactor

--- index.js
+++ index.js
@@ -25,35 +25,37 @@ const puppeteer = require('puppeteer-core');
     return `https://www.x-kom.pl/g-5/c/345-karty-graficzne.html?page=${pageNumber}&f%5B1702%5D%5B178106%5D=1&f%5B1702%5D%5B178114%5D=1&f%5B1702%5D%5B178141%5D=1&f%5B1702%5D%5B186579%5D=1&f%5B1702%5D%5B191561%5D=1&f%5B1702%5D%5B204166%5D=1&f%5B1702%5D%5B204758%5D=1`;
   };
 
-  // pagination selector: #listing-container-wrapper > div.hqmb1u-0.bfSNKf > div.hqmb1u-3.iBNYRN > div > div.hqmb1u-7.kbjFMb
   const paginationParameters = await page.evaluate(() => {
-    return Array.from(
-      document.querySelectorAll(
-        '#listing-container-wrapper > div.hqmb1u-0.bfSNKf > div.hqmb1u-3.iBNYRN > div > div.hqmb1u-7.kbjFMb'
-      )
-    ).map((element) => {
-      if (element) {
-        const [current, all] = element.textContent.split(' z ');
-        const [_, last] = current.split('-');
+    const paginationSelector = `#listing-container-wrapper > div.hqmb1u-0.bfSNKf > div.hqmb1u-3.iBNYRN > div > div.hqmb1u-7.kbjFMb`;
+
+    return Array.from(document.querySelectorAll(paginationSelector)).map(
+      (element) => {
+        if (element) {
+          const [current, all] = element.textContent.split(' z ');
+          const [_, last] = current.split('-');
+
+          return {
+            success: true,
+            current,
+            all,
+            pages: Math.ceil(Number(all) / Number(last)),
+          };
+        }
 
         return {
-          success: true,
-          current,
-          all,
-          pages: Math.ceil(Number(all) / Number(last)),
+          success: false,
         };
       }
-
-      return {
-        success: false,
-      };
-    });
+    );
   });
 
   console.log(paginationParameters);
 
   if (paginationParameters[0].success) {
-    const products = [];
+    const products = {
+      items: [],
+      total: 0
+    };
     // TODO scrap data from first page...
 
     // TODO jezeli stron jest wiecej to powtorz czynnosc dla reszty stron i doklej wynik....
@@ -61,6 +63,10 @@ const puppeteer = require('puppeteer-core');
       for (let index = 2; index <= paginationParameters[0].pages; index++) {
         await page.goto(getLinkWithPaginationParam(index));
         await page.screenshot({ path: `page-${index}.png`, fullPage: true });
+        const result = await getResults(page);
+        products.items.push(...result);
+        products.total = products.items.length;
+        console.log(products);
       }
       // TODO Scrap data
       //const result = await page.evaluate(() => {});
@@ -74,7 +80,7 @@ const puppeteer = require('puppeteer-core');
   /* const response = await page.evaluate(() => {
     return Array.from(
       document.querySelectorAll(
-        '#listing-container > div > div > div.sc-1yu46qn-4.zZmhy.sc-2ride2-0.eYsBmG > div.sc-1yu46qn-10.iQhjQS > div > a > h3 > span'
+        productSelector
       )
     ).map((element) => {
       return {
@@ -88,4 +94,16 @@ const puppeteer = require('puppeteer-core');
   await browser.close();
 })();
 
-const getRestults = async (page) => {};
+const getResults = async (page, pageNumber) =>
+  await page.evaluate(() => {
+    const productSelector = `#listing-container > div > div > div.sc-1yu46qn-4.zZmhy.sc-2ride2-0.eYsBmG > div.sc-1yu46qn-10.iQhjQS > div > a > h3 > span`;
+
+    return Array.from(document.querySelectorAll(productSelector)).map(
+      (element) => {
+        return {
+          id: 
+          name: element.textContent,
+        };
+      }
+    );
+  });
