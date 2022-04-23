9ccfb3880575222e1be92f4e4fc4dd6fd70fc7eb
Vincer0 <pasierb.adam@gmail.com>
2022-04-23 11:11:37 +0200

    feature server: add cors

--- server/server.js
+++ server/server.js
@@ -2,15 +2,48 @@
 const scrapper = require('x-kom-scrapper');
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get('/', async (req, res) => {
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+  })
+);
+
+app.get('/scrap-x', async (req, res) => {
   const scrapData = await scrapper();
   console.log(scrapData);
-  res.send('Done');
+  res.send(scrapData);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+/* {
+  paginationParameters: [ { success: true, current: '1-30', all: '77', pages: 3 } ],
+  products: {
+    items: [
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object], [Object], [Object], [Object],
+      [Object], [Object]
+    ],
+    total: 77
+  },
+  message: 'Success'
+} */
